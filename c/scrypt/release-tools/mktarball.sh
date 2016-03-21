94da4aaea3018b3ea58d994422bea82b20946e66
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-21 16:48:27 -0700

    Added git upstream repos for C sources of fastpbkdf2, py-bcrypt and scrypt

---
+++ c/scrypt/release-tools/mktarball.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+VERSION=$1
+if [ -z $VERSION ]; then
+	echo "Please specify the version number"
+	exit 1
+fi
+DESTDIR=scrypt-${VERSION}
+RELEASEDATE=`date "+%B %d, %Y"`
+
+# Copy bits in
+mkdir ${DESTDIR} ${DESTDIR}/autocrap
+cp scrypt_platform.h main.c FORMAT ${DESTDIR}
+cp Makefile.am configure.ac .autom4te.cfg ${DESTDIR}
+cp Makefile.am configure.ac ${DESTDIR}/autocrap
+cp -R lib libcperciva ${DESTDIR}
+# Copy with substitution
+sed -e "s/@DATE@/$RELEASEDATE/" < scrypt.1 > ${DESTDIR}/scrypt.1
+
+# Generate autotools files
+( cd ${DESTDIR}
+echo -n ${VERSION} > scrypt-version
+autoreconf -i
+rm .autom4te.cfg Makefile.am aclocal.m4 configure.ac scrypt-version )
+
+# Create tarball
+tar -czf ${DESTDIR}.tgz ${DESTDIR}
+rm -r ${DESTDIR}
